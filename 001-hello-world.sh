c7060387ad63ef7fdfb16a39f6a70e89d994d0c0
neomn110 <neomn110@gmail.com>
2024-07-01 21:52:55 +0330

    init: project initialized with creating a hello world script

---
+++ 001-hello-world.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# the above line is called shebang , it specifies which interpreter must be used to interpret this script
+
+echo 'hello world'
+
+echo 'this is the current directory:'
+pwd
